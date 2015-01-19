46f95b20f5881298775d20efd6377db4136538f5
Villanuevand <andresvillanuevab@gmail.com>
2015-01-18 20:47:12 -0430

    Seccion Standings

---
+++ standings/standings.controller.js
@@ -0,0 +1,31 @@
+(function(){
+	'use strict';
+	angular
+		.module('f1App')
+		.controller('StandingsController',Home);
+
+	Home.$injector = ['$q', '$log', 'ErgastService'];
+	function Home($q, $log, ErgastService){
+		var defer = $q.defer();
+		var vm = this;
+			vm.title = 'Driver Standings';
+			vm.standings = null;
+
+
+		execute();
+
+		function execute(){
+			getdriverStandings();
+		}		
+
+		function getdriverStandings(){			
+			return ErgastService.driverStandings()
+				.then(function(response){					
+					vm.standings = response;
+					return vm.standings;				
+				})
+		}
+
+		
+	}
+})();
