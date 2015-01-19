3f3e06ba0c3be935213a631259f777e65d4b37ae
Villanuevand <andresvillanuevab@gmail.com>
2015-01-18 20:46:18 -0430

    Archivo App.js, agregada sección Standings

---
+++ app.js
@@ -0,0 +1,26 @@
+// App.js - Configuración de la aplicación
+(function(){
+	'use strict';
+	angular
+		.module('f1App',[
+			'ngRoute'
+		])
+		.config(config);
+
+	config.$injector = ['$routeProvider'];
+
+	function config($routeProvider){
+		$routeProvider
+		.when('/',{
+			templateUrl : 'home/home.html',
+			controller : 'HomeController',
+			controllerAs : 'home'
+		})
+		.when('/standings',{
+			templateUrl : 'standings/standings.html',
+			controller : 'StandingsController',
+			controllerAs : 'list'
+		});		
+	}
+
+})();
