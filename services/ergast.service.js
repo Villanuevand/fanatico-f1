f8c3a87f4e3c52dfc2d9fa3157d088ae2244a363
Villanuevand <andresvillanuevab@gmail.com>
2015-01-18 22:01:06 -0430

    Actualizado el servicio con la funcionalidad de Carreras

--- services/ergast.service.js
+++ services/ergast.service.js
@@ -5,12 +5,13 @@
 		.module('f1App')
 		.service('ErgastService', Ergast)
 		.constant('URLS', {
-			'DRIVER_STANDINGS' : 'http://ergast.com/api/f1/2014/driverStandings.json'
+			'DRIVER_STANDINGS' : 'http://ergast.com/api/f1/2014/driverStandings.json',
+			'RACE_SCHEDULE' : 'http://ergast.com/api/f1/2015.json'
 		});
 
-	Ergast.$injector = ['$http','$q','$log','URLS'];
+	Ergast.$injector = ['$http','$log','URLS'];
 	// Driver Standings
-	function Ergast($http, $q, $log, URLS){
+	function Ergast($http, $log, URLS){
 		this.driverStandings = function(){			
 			var request = $http({
 				method : 'GET',
@@ -20,7 +21,15 @@
 				return response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
 			});			
 		};
-		
+		this.raceSchedule = function(){
+			var request = $http({
+				method : 'GET',
+				url : URLS.RACE_SCHEDULE
+			});
+			return request.then(function(response){					
+				return response.data.MRData.RaceTable.Races;
+			});
+		}
 	}
 
 
