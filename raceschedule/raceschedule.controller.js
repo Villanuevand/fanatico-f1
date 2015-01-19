c1567b91e86def6ec43bf4939eb628499fa344b0
Villanuevand <andresvillanuevab@gmail.com>
2015-01-18 22:02:34 -0430

    Calendario de Carreras

---
+++ raceschedule/raceschedule.controller.js
@@ -0,0 +1,28 @@
+// Race Schedule Controller
+(function(){
+	'use strict';
+	angular
+		.module('f1App')
+		.controller('RaceScheduleController',RaceSchedule);
+
+	RaceSchedule.$injector = ['$q', '$log', 'ErgastService'];
+	function RaceSchedule($q, $log, ErgastService){
+		var vm = this;
+			vm.title = 'Race Schedule';
+			vm.schedule = null;
+
+		execute();
+
+		function execute(){
+			getRaceSchedule();		
+		}
+
+		function getRaceSchedule(){
+			return ErgastService.raceSchedule()
+				.then(function(response){					
+					vm.schedule = response;
+					return vm.schedule;
+				})
+		}
+	}
+})();
