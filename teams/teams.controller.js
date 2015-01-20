41d3b14c2474482d1526df9cb88584888e219e1b
Villanuevand <andresvillanuevab@gmail.com>
2015-01-19 20:41:57 -0430

    Seccion Equipos

---
+++ teams/teams.controller.js
@@ -0,0 +1,29 @@
+// Teams Controller
+(function(){
+	'use strict';
+	angular
+		.module('f1App')
+		.controller('TeamsController',Teams);
+
+	Teams.$injector = ['$log','ErgastService'];
+	function Teams($log, ErgastService){
+		var vm = this;
+			vm.title = 'Teams';
+			vm.information = null;
+
+		execute();
+
+		function execute(){
+			getTeams();
+		}
+
+		function getTeams(){
+			return ErgastService.getTeams()
+				.then(function(response){
+					vm.information = response;
+					return vm.information;
+				});
+		}
+
+	}
+})();
