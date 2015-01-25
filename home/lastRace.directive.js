410ddb525ef473eb427be68667ef07b02f5353dc
Villanuevand <andresvillanuevab@gmail.com>
2015-01-25 12:40:20 -0430

    Directiva Ultima Carrera

--- home/lastRace.directive.js
+++ home/lastRace.directive.js
@@ -8,7 +8,9 @@
 	function lastRace(){
 		var directive = {
 			restrict : 'E',
-			templateUrl : 'home/lastRace.html'
+			templateUrl : 'home/lastRace.html',
+			controller : 'HomeController',
+			controllerAs : 'info'
 		};
 		return directive;
 	}
