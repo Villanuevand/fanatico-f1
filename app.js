8b6814fe87500ec6638fc23e9ea4edf017ea6b57
Villanuevand <andresvillanuevab@gmail.com>
2015-04-02 04:52:13 -0430

    Separación del rutas a un archivo propio

--- app.js
+++ app.js
@@ -5,56 +5,5 @@
 		.module('f1App',[
 			'ngRoute',
 			'app.filters'
-		])
-		.config(config);
-
-	config.$injector = ['$routeProvider'];
-
-	function config($routeProvider){
-		$routeProvider
-		.when('/',{
-			templateUrl : 'home/home.html',
-			controller : 'HomeController',
-			controllerAs : 'home'
-		})
-		.when('/estadisticas',{
-			templateUrl : 'standings/standings.html',
-			controller : 'StandingsController',
-			controllerAs : 'list'
-		})
-		.when('/calendario',{
-			templateUrl : 'raceschedule/calendar.html',
-			controller : 'RaceScheduleController',
-			controllerAs : 'race'
-		})
-		.when('/circuito/:circuitId/:roundId',{
-			templateUrl : 'raceschedule/circuit.html',
-			controller : 'CircuitController',
-			controllerAs : 'info'
-		})
-		.when('/equipos',{
-			templateUrl	: 'teams/teams.html',
-			controller : 'TeamsController',
-			controllerAs : 'team'
-		})
-		.when('/equipos/:constructorId',{
-			templateUrl : 'teams/teamDetail.html',
-			controller : 'TeamDetailController',
-			controllerAs : 'team'
-		})
-		.when('/pilotos',{
-			templateUrl : 'drivers/drivers.html',
-			controller : 'DriversController',
-			controllerAs : 'driver'
-		})
-		.when('/pilotos/:driverId',{
-			templateUrl : 'drivers/driverDetail.html',
-			controller : 'driverDetailController',
-			controllerAs : 'driver'
-		})
-		.otherwise({
-			redirecTo : '/'
-		});		
-	}
-
+		]);
 })();
