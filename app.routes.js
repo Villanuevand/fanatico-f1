690c7e7d83a298380d4f7b0e590f21881c918773
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-04-02 13:22:35 -0300

    Comentado html5Mode, temporalmente

--- app.routes.js
+++ app.routes.js
@@ -11,7 +11,7 @@
 
     function configRoutes($stateProvider,$urlRouterProvider,$locationProvider){
         $urlRouterProvider.otherwise('/');
-        $locationProvider.html5Mode(true);
+        //$locationProvider.html5Mode(true);
         $stateProvider
             .state('home',{
                 url : '/',
