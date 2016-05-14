60dd1a7bf12441063ca1ba5bb145076844445a7e
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-14 16:23:43 -0300

    Eliminado live-server, Agregado Browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
 // Gulpfile
+var gulp    = require('gulp'),
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify'),
+    browsersync = require('browser-sync').create();
+
+// Browser Sync
+gulp.task('browser-sync', function () {
+    browsersync.init({
+       server: {
+           baseDir: './'
+       }
+    });
+});
+
+gulp.task('default', ['browser-sync']);
+
