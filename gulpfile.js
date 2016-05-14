6da0ab411512ee171cd954558fe773a7a1ecada2
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-14 17:26:27 -0300

    Gulpfile en construccion

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 var gulp    = require('gulp'),
     concat  = require('gulp-concat'),
     uglify  = require('gulp-uglify'),
-    browsersync = require('browser-sync').create();
+    browsersync = require('browser-sync').create(),
+    sourcemaps = require('gulp-sourcemaps');
 
 // Browser Sync
 gulp.task('browser-sync', function () {
@@ -13,5 +14,16 @@ gulp.task('browser-sync', function () {
     });
 });
 
-gulp.task('default', ['browser-sync']);
+// Js Bundle
+gulp.task('bundleJS', function () {
+    return gulp.src('./fanatico-f1/**/*')
+        .pipe(sourcemaps.init())
+        .pipe(concat('fanaticof1.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('build',['browser-sync','bundleJS']);
+gulp.task('default', ['build']);
 
