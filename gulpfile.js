67117eacadb316ef8d2c749754048f2dae6158aa
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-05-28 17:10:33 -0300

    Crea bundle minificado.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp    = require('gulp'),
     concat  = require('gulp-concat'),
     uglify  = require('gulp-uglify'),
     browsersync = require('browser-sync').create(),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    ngAnnotate = require('gulp-ng-annotate');
 
 // Browser Sync
 gulp.task('browser-sync', function () {
@@ -16,9 +17,18 @@ gulp.task('browser-sync', function () {
 
 // Js Bundle
 gulp.task('bundleJS', function () {
-    return gulp.src('./src/*')
+    var app = './src/app.js',
+        routes = './src/app.routes.js',
+        filter = './src/app.filter.js',
+        controllers = './src/controllers/*.js',
+        services = './src/services/*.js',
+        directives = './src/directives/*.js';
+    return gulp.src([app,routes,filter,services,directives,controllers])
         .pipe(sourcemaps.init())
-        .pipe(concat('fanaticof1.min.js'))
+        .pipe(concat('fanaticof1.min.js',{newLine: ';'}))
+        .pipe(ngAnnotate({
+            single_quotes: true
+        }))
         .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist'))
